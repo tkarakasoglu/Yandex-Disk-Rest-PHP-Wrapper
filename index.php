f6ea649ec7c000b87cab4ae86766dbd81a7df45f
Tuna Karakasoglu <tkarakasoglu@gmail.com>
2015-11-09 01:07:40 +0200

    Implemented generic request method Implemented token method Implemented GetDiskInfromation method

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php require "service/service.php";
 
-$applicationId = "";
-
 $service = new Service;
 
 ///////////
 // LOGIN //
 ///////////
 session_start();
-if (!isset($_SESSION["code"])) {
+if (!isset($_SESSION["token"])) {
   if (isset($_REQUEST["code"])) {
     // LOGGED IN AT THE MOMENT
     $_SESSION['code'] = $_REQUEST["code"];
+    $_SESSION["token"] = $service->getToken();
   } else {
     // IF NOT LOGGED IN
-    echo '<a href="' . $service->authorize($applicationId) . '">Login with Yandex</a>';
+    echo '<a href="' . $service->authorize() . '">Login with Yandex</a>';
+    return;
   }
 } else {
   // ALREADY LOGGED IN
 }
 
+echo $service->getDiskInformation();
+//echo $service->getMetaInformation("");
 
 
 
--- service/service.php
+++ service/service.php
@@ -2,13 +2,63 @@
 class Service {
   private $baseUrl = "https://cloud-api.yandex.net/v1/disk/";
   private $authorizationUrl = "https://oauth.yandex.com/authorize";
+  private $tokenUrl = "https://oauth.yandex.ru/token";
 
-  function authorize($applicationId) {
-    return $this->authorizationUrl . "?response_type=code&" . "client_id=" . $applicationId;
+  // USE YOUR OWN APP ID GENERATED FROM : https://oauth.yandex.com/client/new
+  private $applicationId = "";
+  private $applicationSecretId = "";
+
+  // SOURCE: https://tech.yandex.com/oauth/doc/dg/reference/auto-code-client-docpage/
+  function authorize() {
+    return $this->authorizationUrl . "?response_type=code&" . "client_id=" . $this->applicationId;
   }
 
-  function getDiskInformation() {
+  // SOURCE: https://tech.yandex.com/oauth/doc/dg/reference/console-client-docpage/
+  function getToken() {
+    $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8;';
+    $params = 'grant_type=authorization_code&code=' . $_SESSION['code'] . '&client_id=' . $this->applicationId . '&client_secret=' . $this->applicationSecretId;
+    $ch = curl_init($this->tokenUrl);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 80);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec ($ch);
+    curl_close ($ch);
+    return json_decode($response, true)["access_token"];
+  }
 
+  // SOURCE: https://tech.yandex.com/disk/api/reference/capacity-docpage/
+  function getDiskInformation() {
+    return $this->request($this->baseUrl, "");
+  }
+
+  // SOURCE: https://tech.yandex.com/disk/api/reference/meta-docpage/
+  function getMetaInformation($path) {
+    $params = array("path" => $path);
+    $this->request($this->baseUrl, "resources", $params);
+  }
+
+  function request($host, $path, $params = array()) {
+    // Params are a map from names to values
+    $paramStr = "";
+    foreach ($params as $name) {
+      print_r($name);
+    }
+		$opts = array(
+			'http' => array(
+				'method' => 'GET',
+				'header' => 'Authorization: OAuth ' . $_SESSION['token']
+			)
+		);
+		$_default_opts = stream_context_get_params(stream_context_get_default());
+		$opts = array_merge_recursive($_default_opts['options'], $opts);
+		$context = stream_context_create($opts);
+    echo $host . $path . $paramStr;
+    $response = file_get_contents($host . $path . $paramStr, false, $context);
+    return $response;
   }
 }
 
